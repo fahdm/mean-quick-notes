60ce89476325df63ed8cc729f852723fbcf4d127
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-05-17 13:55:58 -0700

    Delete comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-// All paths start with '/api/users'
+
 
 // POST /api/users (create a user - sign up)
 router.post('/', usersCtrl.create);
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,33 +1,3 @@
-// import { useState } from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import { getUser } from '../../utilities/users-service';
-// import './App.css';
-// import AuthPage from '../AuthPage/AuthPage';
-// import NotesPage from '../NotesPage/NotesPage';
-// import Note from '../Note/Note'
-// import NavBar from '../../components/NavBar/NavBar';
-
-// export default function App() {
-//   const [user, setUser] = useState(getUser());
-
-//   return (
-//     <main className="App">
-//       { user ?
-//           <>
-//             <NavBar user={user} setUser={setUser} />
-//             <Routes>
-//               {/* Route components in here */}
-//               <Route path="/orders/new" element={<NotesPage />} />
-//               <Route path="/orders" element={<Note />} />
-//             </Routes>
-//           </>
-//           :
-//           <AuthPage setUser={setUser} />
-//       }
-//     </main>
-//   );
-// }
-
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
